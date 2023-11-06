c3946b30bb207da95b767ffa69669043cb1dcbeb
Lennon Machado <lennon2049@gmail.com>
2023-11-06 19:32:10 +0000

    feat: barra de menu adicionada

--- src/components/menu/BarraMenu.js
+++ src/components/menu/BarraMenu.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './styles.css'
+import './styles.css';
+
+// Importe os arquivos SVG
+import logoSvg from './logo.svg';
+import searchIconSvg from './search-icon.svg';
+import optionsIconSvg from './options-icon.svg';
+import userIconSvg from './user-icon.svg';
+import shopIconSvg from './shop-icon.svg';
+
 function BarraMenu() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Página Inicial</Link>
-                    <Link to="/login">login</Link>
-                    <Link to="/profile">Perfil</Link>
-                    <Link to="/register">Cadastro</Link>
-                    <Link to="/shop">Carrinho</Link>
-                </li>
-            </ul>
+        <nav className="navbar">
+            <div className="left">
+                <div className="logo">
+                    <img src={logoSvg} alt="" />
+                </div>
+            </div>
+            <div className="center">
+                <input type="text" className="search" placeholder="Busque aqui" />
+                <img src={searchIconSvg} className="search-icon" alt="" />
+            </div>
+            <div className="right">
+                <img src={optionsIconSvg} className="icon" alt="" />
+                <img src={userIconSvg} className="icon" alt="" />
+                <img src={shopIconSvg} className="icon" alt="" />
+            </div>
         </nav>
     );
 }
